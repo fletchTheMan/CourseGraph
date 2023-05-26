f2352f219a48ce6418c8e2c1676af3626c4ce536
Fletcher Hammond <hammondfletcher@gmail.com>
2023-05-25 20:05:38 -0400

    Fixed the problems with getting from the website now it works

--- courseParser.py
+++ courseParser.py
@@ -1,5 +1,4 @@
 from bs4 import BeautifulSoup
-import requests
 
 class Course:
     def __init__(self, courseName, courseCredits, courseDesc):
@@ -11,20 +10,20 @@ class Course:
 
 
 
-#courseListFile = open("CourseListFile.txt", "rt")
-#for line in courseListFile:
-#    with open("Courses/" + line.rstrip("\n")) as fp:
-#        soup = BeautifulSoup(fp, "html.parser")
-#        f = open("outfiles/" + line.rstrip(".html\n"), "wt")
-#        courseElements = soup.find_all(class_="courseblock")
-#        for element in courseElements:
-#            f.write(element.find(class_="courseblocktitle").get_text() + "\n")
-#            f.write(element.find(class_="courseblockdesc").get_text() + "\n")
-#            courseElementsExtra = element.find_all(class_="courseblockextra")
-#            for extraElement in courseElementsExtra:
-#                f.write(extraElement.get_text() + "\n")
-#            f.write("\n")
-#        f.close()
+courseListFile = open("CourseListFile.txt", "rt")
+for line in courseListFile:
+    with open("Courses/" + line.rstrip("\n")) as fp:
+        soup = BeautifulSoup(fp, "html.parser")
+        f = open("outfiles/" + line.rstrip(".html\n"), "wt")
+        courseElements = soup.find_all(class_="courseblock")
+        for element in courseElements:
+            f.write(element.find(class_="courseblocktitle").get_text() + "\n")
+            f.write(element.find(class_="courseblockdesc").get_text() + "\n")
+            courseElementsExtra = element.find_all(class_="courseblockextra")
+            for extraElement in courseElementsExtra:
+                f.write(extraElement.get_text() + "\n")
+            f.write("\n")
+        f.close()
 
 
 
--- webscraper.py
+++ webscraper.py
@@ -1,16 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
-baseURL = 'https://catalog.gmu.edu/' 
-url = baseURL + 'courses/'
+baseURL = 'https://catalog.gmu.edu' 
+url = baseURL + '/courses/'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 current_tag = soup.find(id="/courses/")
 coursePages = current_tag.find_all('a')
 for coursePage in coursePages:
     newURL = baseURL + coursePage.get('href')
-    f = open("Courses/" + coursePage.string, "wt")
-    newPage = requests.get(newURL)
-    content = (str)(newPage.content) 
-    f.write(content)
-    f.close()
+    if(coursePage.string.find("Honors") != -1):
+        continue
+    else:
+        f = open("Courses/" + coursePage.string  + ".html", "wt");
+        newPage = requests.get(newURL)
+        f.write(newPage.text)
+        f.close()
